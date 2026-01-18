50c829a3bd508e5313eb7cdd6f5f5902870eb747
Bautista Marquez Odera <bau.marquez@hotmail.com>
2026-01-18 13:22:08 -0300

    Config para Swagger

--- src/main/java/com/gestion/erp/config/SecurityConfig.java
+++ src/main/java/com/gestion/erp/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(req ->
                 req.requestMatchers("/api/v1/auth/**").permitAll()
+                    .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                    .anyRequest().authenticated()
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
